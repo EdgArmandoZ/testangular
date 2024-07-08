78a5b4681224d05bab68a47f8c298a0f56cc70cc
Edgarwebb <161100457+Edgarwebb@users.noreply.github.com>
2024-07-08 16:30:34 -0600

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FComponent } from './f/f.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [FComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
